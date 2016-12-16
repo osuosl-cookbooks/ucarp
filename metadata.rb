144006a02294ce47c28a64ab833efcacc4143ad1
Samarendra M H <samarendra@osuosl.org>
2016-12-16 15:49:09 -0800

    Locking down dependency on 1.0.2 of yum-epel cookbook
    Version 2.0.0 results in an issue for which no workaround was found.
    Reported at https://github.com/chef-cookbooks/yum-epel/issues/41

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email "eheydrick@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures ucarp"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
-depends 'yum-epel'
+depends 'yum-epel','= 1.0.2'
 
 supports 'debian'
 supports 'ubuntu'
