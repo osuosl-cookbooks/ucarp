f98afed274e1255f7bb03bc0898cde3d4efec28b
Manatee <github@osuosl.org>
2017-01-06 23:54:52 +0000

    Automatic patch-level version bump to v1.0.3 by Jenkins

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "eheydrick@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures ucarp"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 issues_url       "https://github.com/osuosl-cookbooks/ucarp/issues"
 source_url       "https://github.com/osuosl-cookbooks/ucarp"
 
